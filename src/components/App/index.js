77d10922a3fcf0c74d47145fe615c8d0264b2103
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2019-01-03 02:58:36 +0200

    night-commit

---
+++ src/components/App/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import Intro from '../Intro';
+import 'whatwg-fetch';
+class App extends Component {
+  
+  state = {
+    series:[
+
+    ]
+  }
+
+  componentDidMount(){
+   fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+   .then(response => response.json() )
+   .then(json => this.setState({ series: json }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tv Series List</h1>
+        </header>
+        <Intro message="Here you can find all of your loved series"/>
+        The length of the series array - {this.state.series.length}
+      </div>
+    );
+  }
+}
+
+export default App;
