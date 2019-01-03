98b54e31107390d880857e4a63822e6e56718afb
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2019-01-03 19:34:08 +0200

    Morning Commit

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -8,17 +8,21 @@ class Series extends Component {
         ]
       }
     
-      componentDidMount(){
-       fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-       .then(response => response.json() )
-       .then(json => this.setState({ series: json }))
-      }
+      
     
-    
+    onSeriesInputChane = e => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+        .then(response => response.json() )
+        .then(json => this.setState({ series: json }))
+    }
+
     render(){
         return(
             <div>
             The length of the series array - {this.state.series.length}
+            <div>
+                <input type="text" onChange={this.onSeriesInputChane} />
+            </div>
             <SeriesList list={this.state.series} />
             </div>
         )
