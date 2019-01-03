c346a126393533a7c34a3ab7bfa1cfc828c89324
emmanuel39hanks <emmanuel.haankwenda2018@gmail.com>
2019-01-03 19:10:55 +0200

    Morning Commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Intro from '../Intro';
+import Series from '../../containers/Series';
 import 'whatwg-fetch';
 class App extends Component {
   
-  state = {
-    series:[
-
-    ]
-  }
-
-  componentDidMount(){
-   fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-   .then(response => response.json() )
-   .then(json => this.setState({ series: json }))
-  }
 
   render() {
     return (
@@ -23,7 +13,8 @@ class App extends Component {
           <h1 className="App-title">Tv Series List</h1>
         </header>
         <Intro message="Here you can find all of your loved series"/>
-        The length of the series array - {this.state.series.length}
+       
+        <Series />
       </div>
     );
   }
---
+++ src/components/SeriesList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+const SeriesList = (props) =>  {
+    return (
+        <div>
+            <ul className="series-list">
+                {props.list.map(series => (
+                    <li>{series.show.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SeriesList;
